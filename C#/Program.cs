4412f48e305ff4e298c60d867d5ea3ccb66b3c26
LC-system32 <alehamartinenko75@gmail.com>
2025-04-10 23:59:35 +0300

    Add C3 code

---
+++ C#/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading;
+
+namespace LB1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculationThread[] calcThreads = new CalculationThread[3];
+            Thread[] threads = new Thread[3];
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                calcThreads[i] = new CalculationThread(i + 1, 2);
+                threads[i] = new Thread(calcThreads[i].Run);
+                threads[i].Start();
+            }
+
+            int[] delays = new int[] { 100000, 80000, 50000 };
+
+            new Thread(() => StopThread(calcThreads, delays)).Start();
+        }
+
+        static void StopThread(CalculationThread[] threads, int[] delays)
+        {
+            for (int i = 0; i < threads.Length; i++)
+            {
+                int index = i;
+                new Thread(() =>
+                {
+                    Thread.Sleep(delays[index]);
+                    threads[index].Running = false;
+                }).Start();
+            }
+        }
+    }
+
+    class CalculationThread
+    {
+        public int Id { get; }
+        private int Step { get; }
+        public volatile bool Running = true;
+
+        public CalculationThread(int id, int step)
+        {
+            Id = id;
+            Step = step;
+        }
+
+        public void Run()
+        {
+            int sum = 0;
+            int count = 0;
+
+            while (Running)
+            {
+                sum += Step;
+                count++;
+            }
+
+            Console.WriteLine($"ID Thread {Id} sum = {sum} count = {count}");
+        }
+    }
+}
